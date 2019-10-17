785d1fdfdfb6d08bef9ee0c65d2616c12bf505d7
HYC <chenghsienyu@aim.com>
2019-10-17 21:59:49 +0800

    add transplayer files
    add transplayer files

---
+++ app/index.js
@@ -0,0 +1,30 @@
+
+
+const videoElement = document.getElementById("vid");
+
+const { GPU } = require('gpu.js');
+const gpu = new GPU();
+const kernel = gpu.createKernel(function (videoFrame) {
+  const pixel = videoFrame[this.thread.y][this.thread.x];
+  this.color(pixel[0], pixel[1], pixel[2], pixel[3]);
+})
+  .setGraphical(true)
+  .setOutput([100, 100]);
+
+kernel(videoElement);
+
+/* 
+function render() {
+  kernel(videoElement);
+  window.requestAnimationFrame(render);
+}
+
+render(); */
+
+
+videoElement.addEventListener('timeupdate', (event) => {
+  var currentTime = videoElement.currentTime;
+  console.log(currentTime);
+  let ipcRenderer = require('electron').ipcRenderer;
+  ipcRenderer.send('timecode', currentTime);
+});
