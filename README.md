3b4ee556c73c715fee82f163f4fa838ef21328db
HYC <chenghsienyu@aim.com>
2019-11-08 01:33:10 +0800

    add notes

--- README.md
+++ README.md
@@ -15,3 +15,12 @@ npm start
 # or
 yarn start
 ```
+## note
+
+ 1. 瀏覽器視窗的layer順序
+ 2. 廣告問題 安裝extension和新增profile for webdriver
+ 3. 更改瀏覽內容比例 可動態或是由profile設定
+ 4. 背景視窗scroll圖片搜尋   但前方影片視窗繼續播影片
+ 5. google search影片 點最多人點閱 並且跳到連接撥放影片in wide mode
+ 6. youtube search 的改進  年度最多點閱與歷史最多點閱?
+ 7. 圖片搜尋後 開始滑動面至最下層是否可以自動fetch更多(目前時間是夠的),視情況是否增加
--- main.js
+++ main.js
@@ -116,7 +116,8 @@ ipcMain.on('timecode', (event, arg) => {
         //console.log(ctrl_state.stat);
       }else if(currentTime == 60){
         await controller.goTo(4, 'https://www.google.com/search?biw=1440&bih=798&tbm=isch&sxsrf=ACYBGNT_rUSmalUS6R34zPF8dOP-8X2iGw%3A1572500280528&sa=1&ei=OHO6Xc_rH-S2mAWoobRQ&q=map+of+melaka+fort&oq=map+of+melaka+fort&gs_l=img.3...2682.4688..4911...0.0..0.75.359.7......0....1..gws-wiz-img.wi4Ueu-el5k&ved=0ahUKEwjP54SF5MXlAhVkG6YKHagQDQoQ4dUDCAc&uact=5#imgrc=_');
-        await controller.setBrowserRect(4, { x: 0, y: 0, width: 2160, height: 1920 });
+        //await controller.setBrowserRect(4, { x: 0, y: 0, width: 2160, height: 1920 });
+        await controller.maximizeBrowser(4);
         controller.SCROLL_GAP = 1;
         controller.SCROLL_INTERVAL = 5;
         await controller.scrollTo(4, 3000);
@@ -126,7 +127,7 @@ ipcMain.on('timecode', (event, arg) => {
         await controller.setBrowserRect(0, { x: 300, y: 300, width: 1024, height: 768 });
         await controller.clickElement(0, By.id('vidthumb1'),0);
         //???
-        await controller.clickElement(0, By.id('ytp-id-29'));
+        //await controller.clickElement(0, By.id('ytp-id-29'));
       }else if(currentTime == 180){
         await controller.goTo(4, 'https://www.google.com/search?q=pawang&sxsrf=ACYBGNSJ5y1GyTyDLeNGuObzD3qF48m3BQ:1572502741554&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiO8MWa7cXlAhUSyIsBHWepAsMQ_AUIEigB&biw=1440&bih=798#imgrc=_');
         await controller.closeBrowser(0);
@@ -169,21 +170,16 @@ ipcMain.on('timecode', (event, arg) => {
     })();
     previousTime = currentTime;
   }
-  /* if(ctrl_state.stat == 0){
-    event.reply('ctrl', 'pause')
-  }else if(ctrl_state.stat == 1){
-    event.reply('ctrl', 'play');
-  } */
 });
 
 //
-function stateChange(arg){
+/* function stateChange(arg){
   Object.defineProperty(ctrl_state, 'stat', {
     value: arg,
     writable: true
   });
 }
-stateChange(0);
+stateChange(0); */
 //
 // delay
 const delay = (interval) => {
