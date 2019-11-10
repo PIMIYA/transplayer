13523c842ff11fc27cc801ad54816d50e6cd0d47
seanmars <mars0222@gmail.com>
2019-11-10 22:27:59 +0800

    重構並增加 focus browser API

--- test.js
+++ test.js
@@ -7,13 +7,23 @@ const Controller = require('./controller');
 let controller = new Controller();
 controller.useLocalProfile = false;
 
+// delay
+const delay = (interval) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, interval)
+    });
+}
+
 (async () => {
     console.log('start test');
 
     await controller.openBrowser(0);
-    await controller.goTo(0, 'https://www.youtube.com/results?search_query=elephant+violent&sp=CAMSAggF');
-    let url = await controller.getLinkUrl(0, By.id('video-title'), 0);
-    console.log(url+'&wide=1');
+    await delay(3000);
+    await controller.focusBrowser(0);
+
+    // await controller.goTo(0, 'https://www.youtube.com/results?search_query=elephant+violent&sp=CAMSAggF');
+    // let url = await controller.getLinkUrl(0, By.id('video-title'), 0);
+    // console.log(url+'&wide=1');
 
     console.log('end test');
 })();
