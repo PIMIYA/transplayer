ab2fb0b4275a4971d5b635bbacc17e230519d515
seanmars <mars0222@gmail.com>
2019-12-03 23:11:04 +0800

    增加 web-process

---
+++ test-sub-process.js
@@ -0,0 +1,51 @@
+const child_process = require('child_process');
+
+let webProcess = child_process.fork('./web-process.js');
+
+let isCancelRequired = false;
+
+const delay = (interval) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, interval)
+    });
+}
+
+if (process.platform === "win32") {
+    var rl = require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.on("SIGINT", function () {
+        process.emit("SIGINT");
+    });
+}
+
+process.on("SIGINT", function () {
+    // graceful shutdown
+    isCancelRequired = true;
+    if (webProcess) {
+        webProcess.kill();
+    }
+    console.log('shutdown...');
+    process.exit();
+});
+
+let currentTime = 770;
+
+(async () => {
+    while (true) {
+        if (isCancelRequired) {
+            break;
+        }
+
+        if (webProcess.killed) {
+            break;
+        }
+
+        webProcess.send({ currentTime: currentTime });
+
+        await delay(1000);
+        currentTime++;
+    }
+})();
