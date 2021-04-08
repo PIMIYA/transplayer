cb2ba255a1def1b6dc1a0d83fc5a1cf402563b49
HsienYu <chenghsienyu@aim.com>
2021-04-08 16:35:46 +0800

    add setup files

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#update repo
+sudo apt-get update
+sudo apt-get upgrade
+
+#setup node js env
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash;
+source ~/.bashrc;
+nvm install node
+
+#install pm2 g
+sudo npm i pm2 -g
+
+#install packages
+npm install
+
+#install chrome broswer 87
+dpkg -i google-chrome-stable_current_amd64.deb
+
