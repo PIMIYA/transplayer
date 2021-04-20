154c101a69ff627673ff1881f098c67d18b19072
HsienYu <chenghsienyu@aim.com>
2021-04-20 22:06:07 +0800

    update setup

--- setup.sh
+++ setup.sh
@@ -4,17 +4,20 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+sudo apt-get install curl -y
+sudo apt-get install -y -f
+
 #setup node js env
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash;
-source ~/.bashrc;
-nvm install node;
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+nvm install node
 
 #install pm2 g
-sudo npm i pm2 -g;
+sudo npm i pm2 -g
 
 #install packages
-npm install;
+npm install
 
 #install chrome broswer 87
-dpkg -i google-chrome-stable_current_amd64.deb;
+dpkg -i google-chrome-stable_current_amd64.deb
 
